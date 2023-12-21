c4a1b7ae6aa5da617ab0d7104f73f5b1e308bad6
1n1tial <dan.yun0821@gmail.com>
2023-12-21 19:16:14 +0900

    fixes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from classes.game import *
 from examples.agent1 import Agent1
 from examples.agent2 import Agent2
+from examples.agent3 import Agent3
+from examples.agent4 import Agent4
+from examples.agent5 import Agent5
+from examples.agent6 import Agent6
 
-g = Game(Agent1(), Agent2())
+g = Game(Agent3(), Agent5())
 
 g.run(100)
 
