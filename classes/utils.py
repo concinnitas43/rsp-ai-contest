054c9175bb8a7012373c1f9f493e3a0900c53b8e
1n1tial <dan.yun0821@gmail.com>
2023-12-21 18:30:57 +0900

    add utils; hand, match, history

---
+++ classes/utils.py
@@ -0,0 +1,24 @@
+from enum import Enum, auto
+
+class Hand(Enum):
+    R = auto()
+    P = auto()
+    S = auto()
+    
+
+Match = (Hand, Hand)
+
+def evalute(match: Match) -> int:
+    if match[0] == match[1]:
+        return 0
+    elif match[0] == Hand.R and match[1] == Hand.S:
+        return 1
+    elif match[0] == Hand.P and match[1] == Hand.R:
+        return 1
+    elif match[0] == Hand.S and match[1] == Hand.P:
+        return 1
+    else:
+        return -1 
+
+History = list[Match]
+
