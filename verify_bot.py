365f270f0bc3c721b7865195ceb0c26eb4fa9457
concinnitas43 <concinnitas43@gmail.com>
2023-12-21 20:05:16 +0900

    dummies

--- verify_bot.py
+++ verify_bot.py
@@ -6,7 +6,12 @@ from examples.dummies.agent4 import Agent4
 from examples.dummies.agent5 import Agent5
 from examples.dummies.agent6 import Agent6
 
-g = Game(Agent4(), Agent6())
+## FROM YOUR OWN BOT
+from examples.dummies.agent4 import Agent4
+
+dummies = [Agent1(), Agent2(), Agent3(), Agent4(), Agent5(), Agent6()]
+
+myBot = Agent4()
 
 g.run(100)
 
